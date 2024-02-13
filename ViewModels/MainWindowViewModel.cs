bf722e071df50deb5db7b095a90e17a3b7f5871d
Joshua Aquino <email@joshuaaquino.xyz>
2024-02-13 15:13:14 -0500

    test minimal playback

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,28 @@
-﻿namespace Nana.ViewModels;
+﻿using System;
+using LibVLCSharp.Shared;
+using Nana.Views;
 
-public class MainWindowViewModel : ViewModelBase
+namespace Nana.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase, IDisposable
 {
 #pragma warning disable CA1822 // Mark members as static
 //    public string Greeting => "Sorry! This still under construction.";
 #pragma warning restore CA1822 // Mark members as static
+    private readonly LibVLC _libvlc = new LibVLC();
+    public MainWindowViewModel()
+    {
+        MediaPlayer = new MediaPlayer(_libvlc);
+    }
+    public void Play()
+    {
+        using var media = new Media(_libvlc, new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+        MediaPlayer.Play(media);
+    }
+    public MediaPlayer MediaPlayer { get; }
+    public void Dispose()
+    {
+        MediaPlayer?.Dispose();
+        _libvlc?.Dispose();
+    }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Nana.ViewModels;
 
 namespace Nana.Views;
 
@@ -7,5 +10,16 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+    
+    private void OnOpened(object sender, EventArgs e)
+    {
+        var vm = DataContext as MainWindowViewModel;
+        vm?.Play();
     }
 }
