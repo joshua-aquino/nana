5a77255912c4eebda5521c80b7ca906639ed9481
Joshua Aquino <email@joshuaaquino.xyz>
2024-02-22 16:06:46 -0500

    introduce named buttons to home

--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,7 +1,25 @@
+using System.IO;
+using Nana.Models;
+
 namespace Nana.ViewModels;
 public class HomeViewModel : ViewModelBase
 {
-    public void OpenPlayer()
+    public string Dud { get ; set ;}
+    public MediaEntry Dummy { get; }
+    public class MediaEntry
     {
+        private readonly string _path;
+        private readonly string _name;
+        public string Name { get => _name ; }
+        public MediaEntry(string path)
+        {
+            _path = path;
+            _name = _path;
+        }
+    }
+    public HomeViewModel()
+    {
+        Dummy = new("./Assets/orange-tesla.mp4");
+        Dud = "balls";
     }
 }
