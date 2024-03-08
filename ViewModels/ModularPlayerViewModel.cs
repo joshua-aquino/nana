ea0354452edad565d94086f5c2299e2775641bdc
Joshua Aquino <email@joshuaaquino.xyz>
2024-03-08 13:15:26 -0500

    setup proper fast forward and crude time tracking

--- ViewModels/ModularPlayerViewModel.cs
+++ ViewModels/ModularPlayerViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Reactive;
 using System.Threading;
 using LibVLCSharp.Shared;
@@ -8,11 +9,20 @@ namespace Nana.ViewModels
 {
     public class ModularPlayerViewModel : ViewModelBase, IDisposable
     {
+        private string _dummy = "wow";
+        public string Dummy
+        {
+            get => _dummy;
+            set => this.RaiseAndSetIfChanged(ref _dummy, value);
+        }
         private readonly LibVLC _libVlc = new LibVLC();
         private Media? media;
-        private bool isPlaying = false;
-        private Timer timer;
-        private TimerCallback timerCallback;
+        private bool _isPlaying = false;
+        public bool IsPlaying 
+        {
+            get => _isPlaying;
+            set => this.RaiseAndSetIfChanged(ref _isPlaying, value);
+        }
         private TimeSpan _length;
         public TimeSpan Length
         {
@@ -25,14 +35,25 @@ namespace Nana.ViewModels
             get => _currentPosition;
             set => this.RaiseAndSetIfChanged(ref _currentPosition, value);
         }
-        private void AdjustPlayingStatus()
+        private Timer timer;
+        private TimerCallback timerCallback;
+        private void AdjustPlayingStatus(object? sender, EventArgs e) // I want to check playable, but there is no playable event args
         {
+            Console.WriteLine("Event Read");
             if (MediaPlayer.IsPlaying)
-                isPlaying = true;
+            {
+                Console.WriteLine("PlAYING");
+                IsPlaying = true;
                 timer = new Timer(timerCallback, null, 0, 1000);
+            }
             else
-                isPlaying = false;
-                timer?.Dispose;
+            {
+                Console.WriteLine("Stopped");
+                IsPlaying = false;
+                timer?.Dispose();
+            }
+            MediaPlayer.Paused -= AdjustPlayingStatus; 
+            MediaPlayer.Playing -= AdjustPlayingStatus; 
         }
         public void OpenFile(string path)
         {
@@ -46,43 +67,58 @@ namespace Nana.ViewModels
         }
         private void PositionTick(object state)
         {
-            CurrentPosition = CurrentPosition.Add(TimeSpan.FromSeconds(1));
+            CurrentPosition = TimeSpan.FromMilliseconds(MediaPlayer.Time);
         }
+
         public void Play()
         {
             if (media != null)
+            {
+                MediaPlayer.Playing += AdjustPlayingStatus; 
                 MediaPlayer.Play(media);
-            AdjustPlayingStatus();
+            }
         }
+
         public void PlayPause()
         {
-            MediaPlayer.Pause();   
-            AdjustPlayingStatus();
+            if (media != null)
+            {
+                MediaPlayer.Playing += AdjustPlayingStatus; 
+                MediaPlayer.Paused += AdjustPlayingStatus; 
+                MediaPlayer.Pause();   
+            }
         }
         public void Stop()
         {
             MediaPlayer.Stop();
-            AdjustPlayingStatus();
         }
         public void FastForward()
         {
             if (MediaPlayer.IsPlaying)
+            {
+                MediaPlayer.Playing += AdjustPlayingStatus; 
+                MediaPlayer.Paused += AdjustPlayingStatus; 
                 MediaPlayer.Pause();
+            }
             if (MediaPlayer.Position < 0.98)
             {
                 MediaPlayer.Position = (float)(MediaPlayer.Position + 0.01);
             }
-            AdjustPlayingStatus();
+            CurrentPosition = TimeSpan.FromMilliseconds(MediaPlayer.Time);
         }
         public void Rewind()
         {
             if (MediaPlayer.IsPlaying)
+            {
+                MediaPlayer.Playing += AdjustPlayingStatus; 
+                MediaPlayer.Paused += AdjustPlayingStatus; 
                 MediaPlayer.Pause();
+            }
             if (MediaPlayer.Position > 0.02)
             {
                 MediaPlayer.Position = (float)(MediaPlayer.Position - 0.01);
             }
-            AdjustPlayingStatus();
+            CurrentPosition = TimeSpan.FromMilliseconds(MediaPlayer.Time);
         }
         public MediaPlayer MediaPlayer { get; }
         public void Dispose()
